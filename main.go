68c3a22f9e4f517a14ccc4292df986a7dedfa300
Dave Newman <dave@snappyco.de>
2012-09-07 10:47:17 -0700

    jsonify

--- main.go
+++ main.go
@@ -1,30 +1,48 @@
-package pinky
+package main
 
 import (
   	"fmt"
 	"log"
   	"os"
+	"encoding/json"
   	"github.com/alphazero/Go-Redis"
 )
 
-func popRedisQueue(c chan string, client redis.Client, queue string) {
+type Job struct {
+	Name string
+	ServerId string
+}
+
+func popRedisQueue(c chan Job, client redis.Client, queue string) {
 	for {
 		bytes, e := client.Brpop(queue, 20);
 		if e!= nil { log.Println ("error on BRPOP", e); return }
 		
 		// If the pop times out, it just returns the key, no value
 		if len(bytes) > 1 {
-			c <- fmt.Sprintf("%s", bytes[1])
+			// fmt.Sprintf("%s", bytes[1])
+			var m Job
+			json.Unmarshal(bytes[1], &m)
+			c <- m
 		}
 	}
 }
 
-func startWorld() {
-	fmt.Println("Starting world")
+func startWorld(job Job) {
+	fmt.Println("Starting world", job.ServerId)
 }
 
-func stopWorld() {
+func stopWorld(job Job) {
 	fmt.Println("Stopping world")
+}
+
+func createRedisClient(database int) (client redis.Client) {
+	spec := redis.DefaultSpec().Db(database)
+	client, e := redis.NewSynchClientWithSpec(spec);
+	if e != nil { 
+		panic("failed to create the client") 
+	}
+	return
 }
 
 func main() {
@@ -32,13 +50,11 @@ func main() {
 
 	fmt.Println("box_id: ", boxId)
 
-	spec := redis.DefaultSpec().Db(13)
-	client, e := redis.NewSynchClientWithSpec (spec);
-	if e != nil { log.Println ("failed to create the client", e); return }
+	client := createRedisClient(13)
 
 	boxQueueKey := fmt.Sprintf("box/%s/queue", boxId)
 	
-	jobChannel := make(chan string)
+	jobChannel := make(chan Job)
 
 	go popRedisQueue(jobChannel, client, boxQueueKey)
 	
@@ -46,9 +62,9 @@ func main() {
 		job := <- jobChannel
 		fmt.Println(job)
 	
-		switch job {
-		case "start": startWorld()
-		case "stop": stopWorld()
+		switch job.Name {
+		case "start": startWorld(job)
+		case "stop": stopWorld(job)
 		default: fmt.Println("Unknown job", job)
 		}
 	}
--- main_test.go
+++ main_test.go
@@ -1,4 +1,4 @@
-package pinky
+package main
 
 import "testing"
 
